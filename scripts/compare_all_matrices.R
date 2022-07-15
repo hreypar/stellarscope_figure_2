f2742ab4d0d42ab5e6f21b35d62ca334bfa4d4c1
Helena Reyes Gopar <hreypar@gmail.com>
2022-07-15 09:32:33 -0400

    create logical table and upsetR

--- scripts/compare_all_matrices.R
+++ scripts/compare_all_matrices.R
@@ -11,10 +11,10 @@
 # load libraries
 #library(Matrix)
 library(magrittr)
-#library(UpSetR)
+library(UpSetR)
 library(ggplot2)
 library(ggVennDiagram)
-#library(grid)
+library(grid)
 #library(ggrepel)
 #library(gplots)
 library(Seurat)
@@ -28,7 +28,7 @@ te_subset <- function(fnames) {
 
 # function to plot a venn diagram of the TE features and cells in 
 # (1) counts matrix, (2) raw qc seurat, (3) normalized seurat
-compare_matrices_fun <- function(sample.name, stellarscope.mode, reassign.method = "exclude") {
+compare_matrices_fun <- function(sample.name, stellarscope.mode, reassign.method = "exclude", clusters.source="") {
   # read in files
   file.path("data", "counts_matrix_R",
             sample.name,
@@ -86,7 +86,8 @@ compare_matrices_fun <- function(sample.name, stellarscope.mode, reassign.method
   
   dev.off()
   
-  ##### save an object with the sets 
+  ##### save an object with the sets
+  ## features
   TE_features=te_subset(rownames(counts.mat))
 
   rawSeurat_features = gsub("-", "_", te_subset(rownames(raw.seurat)))
@@ -94,15 +95,39 @@ compare_matrices_fun <- function(sample.name, stellarscope.mode, reassign.method
   
   if(length(setdiff(rawSeurat_features[rawSeurat_features %in% TE_features],
           normSeurat_features[normSeurat_features %in% TE_features])) > 0) {
-    stop("rawSeurat and normSeurat don't have the same features\n")
+    stop("rawSeurat and normSeurat don't have the same features!\n")
   }
   
   # since we checked and the counts matrices all have the full TE annotation set
   # and both raw seurat and analysed seurat have the same TE sets
-  detection <- data.frame(TE_features, TE_features %in% rawSeurat_features)
-  colnames(detection) <- c("TE_transcripts", paste(sample.name, stellarscope.mode, sep="_"))
+  detection <- data.frame(TE_features, TE_features %in% normSeurat_features)
+  if(stellarscope.mode == "clusters") {
+    colnames(detection) <- c("TE_transcripts", paste(sample.name, "celltypes", clusters.source, sep="_"))
+  } else {
+    colnames(detection) <- c("TE_transcripts", paste(sample.name, stellarscope.mode, sep="_"))
+  }
   
-  return(detection)
+  ## cells
+  initial_cells=colnames(counts.mat)
+  
+  if(length(setdiff(colnames(raw.seurat), colnames(an.seurat))) > 0) {
+    stop("rawSeurat and normSeurat don't have the same cells!\n")
+  }
+  
+  true_cells = data.frame(initial_cells, initial_cells %in% colnames(an.seurat))
+  
+  if(stellarscope.mode == "clusters") {
+    colnames(true_cells) <- c(paste0("initial_cells_", sample.name),
+                              paste0("filtered_cells_", stellarscope.mode, "_", clusters.source))
+  } else {
+    colnames(true_cells) <- c(paste0("initial_cells_", sample.name),
+                              paste0("filtered_cells_", stellarscope.mode))
+  }
+  
+  out <- list(detection, true_cells)
+  names(out) <- c("te", "cells") # DO I HAVE TO NAME THESE SPECIFICALLY?
+  
+  return(out)
 }
 
 ################################################################################
@@ -110,15 +135,78 @@ compare_matrices_fun <- function(sample.name, stellarscope.mode, reassign.method
 #sample.name = "500_PBMC_3p_LT_Chromium_X"
 #reassign.method = "exclude"
 #stellarscope.mode = "pseudobulk"
+#clusters.source = "10X"
+
 pbmc.datasets <- c("500_PBMC_3p_LT_Chromium_X", "10k_PBMC_3p_nextgem_Chromium_X", "20k_PBMC_3p_HT_nextgem_Chromium_X")
 
 detected_pseudobulk <- lapply(pbmc.datasets, compare_matrices_fun, stellarscope.mode = "pseudobulk")
+#names(detected_pseudobulk) <- pbmc.datasets
 detected_individual <- lapply(pbmc.datasets, compare_matrices_fun, stellarscope.mode = "individual")
-detected_clusters <- lapply(pbmc.datasets, compare_matrices_fun, stellarscope.mode = "clusters")
+detected_clusters <- lapply(pbmc.datasets, compare_matrices_fun, stellarscope.mode = "clusters", clusters.source="10X")
+
+##### check that the features are in the same order
+pse <- do.call(cbind, lapply(detected_pseudobulk, function(x) {
+  x[["te"]]$TE_transcripts
+}))
+
+ind <- do.call(cbind, lapply(detected_individual, function(x) {
+  x[["te"]]$TE_transcripts
+}))
+
+clu <- do.call(cbind, lapply(detected_clusters, function(x) {
+  x[["te"]]$TE_transcripts
+}))
+
+all <- cbind(pse, ind, clu)
+all <- apply(all, 1, unique)
+
+if(length(all) != 27812) {
+  stop("The order of features doesn't match!\n")
+} else {
+  rm(pse, ind, clu, all)
+}
+
+# create features and cells tables and save
+pbmc_features_table <- cbind(detected_pseudobulk[[1]]$te,
+                             detected_individual[[1]]$te,
+                             detected_clusters[[1]]$te,
+                             detected_pseudobulk[[2]]$te,
+                             detected_individual[[2]]$te,
+                             detected_clusters[[2]]$te,
+                             detected_pseudobulk[[3]]$te,
+                             detected_individual[[3]]$te,
+                             detected_clusters[[3]]$te)
+
+pbmc_features_table <- pbmc_features_table[, !duplicated(colnames(pbmc_features_table))]
+write.csv(x = pbmc_features_table, file = "results/comparison_features_cells/pbmc_datasets_detected_features.csv", quote = F, row.names = F)
 
 
 ################################################################################
 ######################### compare all matrices
+##### plot upsetR
+
+pbmc_upset <- lapply(colnames(pbmc_features_table), function(x) {
+  if(x == "TE_transcripts") {
+    return(pbmc_features_table[,`x`])
+  } else{
+    ind <- pbmc_features_table[,`x`]
+    return(pbmc_features_table[ind, "TE_transcripts"])
+  }
+})
+
+names(pbmc_upset) <- colnames(pbmc_features_table)
+
+png("results/comparison_features_cells/pbmc_datasets_detected_features.png",
+    width = 26, height = 12, units = "in", res=300)
+
+upset(fromList(pbmc_upset),
+      order.by = c("freq"), point.size = 3.5, nsets = 20, #mb.ratio = c(0.45, 0.55),
+      mainbar.y.label = "Detected Transposable Elements",  
+      text.scale = c(2, 1.35, 1.35, 1.25, 1.65, 1.95)
+) 
+grid.text("Comparison of TE Features", x = 0.65, y=0.95, gp=gpar(fontsize=20))
+
+dev.off()
 
 
 
